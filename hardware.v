1929b4449e93db01755826ae1570781a960244b6
Gazal Garg <gazal24@gmail.com>
2012-04-24 14:51:49 +0530

    code for Nmix added.

--- hardware.v
+++ hardware.v
@@ -26,20 +26,86 @@
 // endmodule // xora_tb
 
 
-module add_bits (a, b, c);
-   input [31:0] a, b;
-   output [31:0] c;
-   assign c = a & b;
+module add_bits (X, R, Y, clk, reset, dirty);
+   input  [31:0] X, R;
+   output [31:0] Y;
+   input 	 clk,reset;
+   output 	 dirty;
+   
+   reg [31:0] 	 Y;
+   reg [5:0] 	 i;
+   reg [5:0] 	 j;
+   reg 		 dirty;
+
+   always @(posedge clk or posedge reset)
+     begin
+	$display("meh");
+	if(reset)
+	  begin
+	     i = 0;
+	     dirty = 1;
+	  end
+	else
+	  begin
+	     if(dirty)
+	       begin
+		  if(j==0) 
+		    // initialize
+		    begin
+		       Y[i] = (X[0]&R[0]) ^ (R[i] & R[i-1]) ^ (X[i] & X[i-1]);
+		       j = 1;
+		    end
+		  else
+		    begin
+		       if(j == i)
+			 begin
+			    i = i + 1;
+			    j = 0;
+			    if(i == 32)
+			      begin
+				 dirty = 0;
+			      end
+			 end
+		       else
+			 // main code
+			 begin
+			    $display("meh");
+			    Y[i] = Y[i] ^ X[j]&R[j];
+			    j = j+1;
+			 end
+		    end // else: !if(j==0)
+	       end // if (dirty)
+	  end // else: !if(reset)
+     end // always (posedge clk or posedge reset)
 endmodule // adder
 
-module add_tb;
-   reg [31:0] X,R;
+module dff_sync_reset (data, clk, reset, q);
+   input  data, clk, reset ; 
+   output q;
+   reg 	  q;
+   always @ ( posedge clk)
+     if (~reset) begin
+	q <= 1'b0;
+     end  else begin
+	q <= data;
+     end
+endmodule //End Of Module dff_sync_reset
+
+
+module add_bits_tb;
+   reg  [31:0] X,R;
    wire [31:0] Y;
+   reg 	       clk;
+   
+   initial clk = 0; 
+   always #1 clk = ~clk; 
+   
    initial begin
       $monitor("X=%b,\nR=%b,\nY=%b,",X,R,Y);
       X = 1024 * 1024 + 2378923475;
       R = 84765823;
       $finish;
    end
-   adder A1(.a(X), .b(R), .c(Y));
+   add_bits A1(.X(X), .R(R), .Y(Y), .clk(clk));
+
 endmodule // add_tb
