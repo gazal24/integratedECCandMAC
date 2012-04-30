4c35d14610cc4d92cc382ce6d4f7d909391ca99e
Gazal Garg <gazal24@gmail.com>
2012-04-30 07:34:33 +0530

    ca_tb.v working. Code for FILE READING for input also in place :-D

--- ca_tb.v
+++ ca_tb.v
@@ -1,26 +1,54 @@
 module ca_tb();
-   reg clk,reset,preset,d;
-   wire q;
+   reg clk,reset,preset;
+   reg [0:7] in;
+   wire [0:7] out;
 
-   dff uud(.clk(clk),.reset(reset),.preset(preset),.d(d), .q(q));
+   // for file
+   reg [0:7]  data;
+   reg [0:7]  count;
+   integer    fd;
+   integer    code, dummy;
+   reg [8*10:1] str;
+   // end for flle
+   
+   CA uud(.clk(clk),.reset(reset),.in(in),.out(out));
    initial begin
-      $monitor("CLK = %b RESET = %b PRESET = %b D = %b Q = %b", clk,reset,preset,d,q);
+      $monitor("CLK = %b RESET = %b PRESET = %b D = %b Q = %b", clk,reset,preset,in,out);
       clk = 0;
       #1 reset  = 0;
       preset = 0;
-      d = 0;
+      in = 0;
+      fd = $fopen("input.txt","r"); 
       #1 reset = 1;
       #2 reset = 0;
       #2 preset = 1;
       #2 preset = 0;
-      #20 $finish;
-      
+      // #20 $finish;
+   end
+   
+   always @(posedge clk) begin
+      data = 0;
+      code = 1;
+      count = 0;
+      $monitor("data = %x", data);
+      while (count < 8) begin
+	 code = $fgets(str, fd);
+         dummy = $sscanf(str, "%x", data);
+	 if(code == 0)
+	   $finish;
+	 else if(data == 0)
+	   in[count] = 0;
+	 else 
+	   in[count] = 1;
+	 count = count + 1;
+      end
    end
    
    always begin
       #1 clk = ~clk;
    end
-   always begin
-     #1 d = ~d;
-   end
+   always
+     #1 in = in + 1;
+   
 endmodule
+
